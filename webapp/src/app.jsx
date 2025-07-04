6c45abd581a60a4b66a38b8634c75d8f3e9582af
fabiianchykm <109103354+fabiianchykm@users.noreply.github.com>
2025-07-04 13:11:27 +0300

    refactor: Move chapters and questions to separate files

--- webapp/src/app.jsx
+++ webapp/src/app.jsx
@@ -1,39 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
 
-// Українські абревіатури та кількість розділів
-const booksData = [
-  { name: 'Бут', chapters: 50 }, { name: 'Вих', chapters: 40 }, { name: 'Лев', chapters: 27 },
-  { name: 'Чис', chapters: 36 }, { name: 'Втор', chapters: 34 }, { name: 'ІсНав', chapters: 24 },
-  { name: 'Суд', chapters: 21 }, { name: 'Рут', chapters: 4 }, { name: '1Сам', chapters: 31 },
-  { name: '2Сам', chapters: 24 }, { name: '1Цар', chapters: 22 }, { name: '2Цар', chapters: 25 },
-  { name: '1Хр', chapters: 29 }, { name: '2Хр', chapters: 36 }, { name: 'Езд', chapters: 10 },
-  { name: 'Неєм', chapters: 13 }, { name: 'Ест', chapters: 10 }, { name: 'Йов', chapters: 42 },
-  { name: 'Пс', chapters: 150 }, { name: 'Прип', chapters: 31 }, { name: 'Екл', chapters: 12 },
-  { name: 'Пісн', chapters: 8 }, { name: 'Іс', chapters: 66 }, { name: 'Єр', chapters: 52 },
-  { name: 'Плач', chapters: 5 }, { name: 'Єз', chapters: 48 }, { name: 'Дан', chapters: 12 },
-  { name: 'Ос', chapters: 14 }, { name: 'Йоіл', chapters: 3 }, { name: 'Ам', chapters: 9 },
-  { name: 'Овд', chapters: 1 }, { name: 'Йона', chapters: 4 }, { name: 'Мих', chapters: 7 },
-  { name: 'Наум', chapters: 3 }, { name: 'Ав', chapters: 3 }, { name: 'Соф', chapters: 3 },
-  { name: 'Аг', chapters: 2 }, { name: 'Зах', chapters: 14 }, { name: 'Мал', chapters: 4 },
-  { name: 'Мт', chapters: 28 }, { name: 'Мр', chapters: 16 }, { name: 'Лк', chapters: 24 },
-  { name: 'Ів', chapters: 21 }, { name: 'Дії', chapters: 28 }, { name: 'Рим', chapters: 16 },
-  { name: '1Кор', chapters: 16 }, { name: '2Кор', chapters: 13 }, { name: 'Гал', chapters: 6 },
-  { name: 'Еф', chapters: 6 }, { name: 'Флп', chapters: 4 }, { name: 'Кол', chapters: 4 },
-  { name: '1Сол', chapters: 5 }, { name: '2Сол', chapters: 3 }, { name: '1Тим', chapters: 6 },
-  { name: '2Тим', chapters: 4 }, { name: 'Тит', chapters: 3 }, { name: 'Флм', chapters: 1 },
-  { name: 'Євр', chapters: 13 }, { name: 'Як', chapters: 5 }, { name: '1Пет', chapters: 5 },
-  { name: '2Пет', chapters: 3 }, { name: '1Ів', chapters: 5 }, { name: '2Ів', chapters: 1 },
-  { name: '3Ів', chapters: 1 }, { name: 'Юд', chapters: 1 }, { name: 'Об', chapters: 22 }
-];
-
-// Дані для контрольних питань
-const quizData = {
-    'Лк-5': {
-        question: 'Кого Ісус покликав, коли сказав: "Ідіть за Мною, і Я зроблю вас ловцями людей"?',
-        options: ['Митників', 'Рибалок', 'Фарисеїв', 'Пастухів'],
-        correctAnswer: 'Рибалок'
-    }
-};
+import { booksData, quizData } from './data.js'; // Імпортуємо дані
 
 export function App() {
   const [activeBook, setActiveBook] = useState(booksData[0]); 
